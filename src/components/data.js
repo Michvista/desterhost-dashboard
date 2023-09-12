dd3adb3c331a0557bcc82c75846ca83efc3a52ee
Tolu <70198065+tolu-ayobami@users.noreply.github.com>
2023-09-12 15:29:48 +0100

    Add files via upload

--- src/components/data.js
+++ src/components/data.js
@@ -1,14 +1,98 @@
-
-
-
+import React from "react";
+import edi from "../images/edit.png";
+import del from "../images/del.png";
 const review = [
 
-    ];
+    {
+      id: 1,
+      name: "Server down time",
+      category: 'Domains',
+      ticketid:"#1234",
+      date:"12/04/2024",
+      status:"Pending",
+      editimg:edi,
+      delimg:del,
+      
+    },
+    {
+      id: 2,
+      name: "Server down time",
+      category: 'Domains',
+      ticketid:"#1234",
+      date:"12/04/2024",
+      status:"Pending",
+      editimg:edi,
+      delimg:del,
+      
+    },
+    {
+      id: 3,
+      name: "Server down time",
+      category: 'Domains',
+      ticketid:"#1234",
+      date:"12/04/2024",
+      status:"Pending",
+      editimg:edi,
+      delimg:del,
+      
+    },
+    {
+      id: 4,
+      name: "Server down time",
+      category: 'Domains',
+      ticketid:"#1234",
+      date:"12/04/2024",
+      status:"Pending",
+      editimg:edi,
+      delimg:del,
+     
+    },
+
+    {
+        id: 4,
+        name: "Server down time",
+        category: 'Domains',
+        ticketid:"#1234",
+        date:"12/04/2024",
+        status:"Pending",
+        editimg:edi,
+        delimg:del,
+       
+      },
+
+      {
+        id: 5,
+        name: "Server down time",
+        category: 'Domains',
+        ticketid:"#1234",
+        date:"12/04/2024",
+        status:"Pending",
+        editimg:edi,
+        delimg:del,
+       
+      },
+
+      {
+      id: 6,
+      name: "Server down time",
+      category: 'Domains',
+      ticketid:"#1234",
+      date:"12/04/2024",
+      status:"Pending",
+      editimg:edi,
+      delimg:del,
+     
+    },
     
-    export default review;
-
-
-
+      ];
+      
+      export default review;
+  
+  
+  
+  
+  
+      
 
 
     
